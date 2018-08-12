acdd591fe62bbae2fcb7a6794c87df3d19c95d47
Santiago Pérez <santiagoperez.-@hotmail.com>
2018-08-12 05:07:19 -0300

    Agregada texto que muestra la version de la build.

---
+++ Thesis/Assets/Scripts/Main Menu/BuildVersion.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class BuildVersion : MonoBehaviour {
+    
+	void Awake()
+    {
+        GetComponent<Text>().text = "v" + Application.version;
+    }
+}
