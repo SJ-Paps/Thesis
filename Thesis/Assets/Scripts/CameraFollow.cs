fadec331d508c92314a0fedbd3914bbe48bcfc2f
Aniluj <julianvolonterio@gmail.com>
2018-09-27 05:24:45 -0300

    Script para el seguimiento del player con la cámara agregado y avance en la creación del nivel
    Se avanzó en la creación del nivel y se reemplazó el tipo de collider y el objeto que lo contenía, que anteriormente existían y funcionaaban como los colliders del mundo. También se agregaron nuevos tilemaps, nuevos tiles, un fondo y nuevas paletas de tiles para poder seguir trabajando a futuro con los elementos requeridos.

---
+++ Thesis/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public float maxMovementInY;
+	public Transform target;
+	private Vector3 posDiff = Vector3.zero;
+
+	void Start () {
+		posDiff = transform.position - target.position;	
+	}
+
+	void Update () {
+		transform.position = target.position + posDiff;
+	}
+}
