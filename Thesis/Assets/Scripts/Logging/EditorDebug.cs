e693156b6d5bace1c33e0ed8879a48008de56255
Sammmte <santiagoperez.-@hotmail.com>
2018-11-16 19:41:38 -0300

    Arreglado pequeño error que enviaba una orden de esconderse cuando en realidad se deseaba empujar un objeto. Quitadas llamadas directas a Debug.Log

--- Thesis/Assets/Scripts/Logging/EditorDebug.cs
+++ Thesis/Assets/Scripts/Logging/EditorDebug.cs
@@ -4,21 +4,21 @@ public class EditorDebug
 {
     public static void Log(object obj)
     {
-#if UNITY_EDITOR && !LOG_OFF
+#if UNITY_EDITOR && LOG
         Debug.Log(obj);
 #endif
     }
 
     public static void LogWarning(object obj)
     {
-#if UNITY_EDITOR && !LOG_OFF
+#if UNITY_EDITOR && LOG
         Debug.LogWarning(obj);
 #endif
     }
 
     public static void DrawLine(Vector3 origin, Vector3 end, Color color)
     {
-#if UNITY_EDITOR && !LOG_OFF
+#if UNITY_EDITOR && LOG
         Debug.DrawLine(origin, end, color);
 #endif
     }
