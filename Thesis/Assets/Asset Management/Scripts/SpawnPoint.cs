59d037854b1eb68bcd80ee31ef86b8bef3c6c1ed
Santiago Pérez <santiagoperez.-@hotmail.com>
2018-11-17 20:26:15 -0300

    Creada herramienta de gizmo para los spawn points de los objetos de la escena

---
+++ Thesis/Assets/Asset Management/Scripts/SpawnPoint.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class SpawnPoint : MonoBehaviour {
+
+    [SerializeField]
+    private SJMonoBehaviour prefab;
+
+    public SJMonoBehaviour Prefab
+    {
+        get
+        {
+            return prefab;
+        }
+    }
+
+    [SerializeField]
+    private Material spriteDefault;
+
+    [SerializeField]
+    private Color colorTransparency;
+
+    [SerializeField]
+    private float colorTransparencyThreshold = 0.1f;
+
+    [SerializeField]
+    private bool flipX, flipY;
+
+    [SerializeField]
+    private int size = 2;
+
+    private Texture2D prefabThumbnail;
+
+    public void Spawn()
+    {
+        Instantiate(prefab, transform.position, prefab.transform.rotation);
+    }
+
+    void OnValidate()
+    {
+        prefabThumbnail = AssetPreview.GetAssetPreview(prefab.gameObject);
+    }
+
+    void OnDrawGizmos()
+    {
+        if(prefab != null)
+        {
+            if (prefabThumbnail != null)
+            {
+                int finalSizeX;
+                int finalSizeY;
+
+                if (flipX)
+                {
+                    finalSizeX = size;
+                }
+                else
+                {
+                    finalSizeX = -size;
+                }
+
+                if(flipY)
+                {
+                    finalSizeY = size;
+                }
+                else
+                {
+                    finalSizeY = -size;
+                }
+
+                if(spriteDefault == null)
+                {
+                    Gizmos.DrawGUITexture(new Rect(0, 0, finalSizeX, finalSizeY), prefabThumbnail);
+                }
+                else
+                {
+                    Gizmos.DrawGUITexture(new Rect(0, 0, finalSizeX, finalSizeY), prefabThumbnail, spriteDefault);
+                }
+                
+            }
+        }
+    }
+}
