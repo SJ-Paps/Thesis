daf9ff2a1c1f447a97b7308602ad41528c7c2058
Santiago Pérez <santiagoperez.-@hotmail.com>
2019-03-10 19:24:31 -0300

    Creada soga mas grande para pruebas. Cambiada la fuerza que aplica el swinging. Mas adelante sera customizable.

--- Thesis/Assets/Scripts/Characters/Tribal/States/Scripts/TribalSwingingRopeState.cs
+++ Thesis/Assets/Scripts/Characters/Tribal/States/Scripts/TribalSwingingRopeState.cs
@@ -12,12 +12,12 @@ public class TribalSwingingRopeState : TribalHSMState
         if (LastEnteringTrigger == Character.Order.MoveLeft)
         {
             Owner.Face(true);
-            Blackboard.ropeHandler.RelativeOther.connectedBody.AddRelativeForce(new Vector2(-Owner.TribalConfigurationData.ClimbForce * 2, 0));
+            Blackboard.ropeHandler.RelativeOther.connectedBody.AddRelativeForce(new Vector2(-Owner.TribalConfigurationData.ClimbForce * 6, 0));
         }
         else if (LastEnteringTrigger == Character.Order.MoveRight)
         {
             Owner.Face(false);
-            Blackboard.ropeHandler.RelativeOther.connectedBody.AddRelativeForce(new Vector2(Owner.TribalConfigurationData.ClimbForce * 2, 0));
+            Blackboard.ropeHandler.RelativeOther.connectedBody.AddRelativeForce(new Vector2(Owner.TribalConfigurationData.ClimbForce * 6, 0));
         }
 
         SendEvent(Character.Order.FinishAction);
