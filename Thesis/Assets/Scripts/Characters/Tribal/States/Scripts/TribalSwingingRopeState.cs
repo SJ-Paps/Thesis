bac526b09d9f18fb34655c8eb16a75f2f14ceff7
Santiago Pérez <santiagoperez.-@hotmail.com>
2019-03-10 19:18:51 -0300

    Los tribal pueden balancearse en las cuerdas.

---
+++ Thesis/Assets/Scripts/Characters/Tribal/States/Scripts/TribalSwingingRopeState.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TribalSwingingRopeState : TribalHSMState
+{
+    protected override void OnEnter()
+    {
+        base.OnEnter();
+
+        if (LastEnteringTrigger == Character.Order.MoveLeft)
+        {
+            Owner.Face(true);
+            Blackboard.ropeHandler.RelativeOther.connectedBody.AddRelativeForce(new Vector2(-Owner.TribalConfigurationData.ClimbForce * 2, 0));
+        }
+        else if (LastEnteringTrigger == Character.Order.MoveRight)
+        {
+            Owner.Face(false);
+            Blackboard.ropeHandler.RelativeOther.connectedBody.AddRelativeForce(new Vector2(Owner.TribalConfigurationData.ClimbForce * 2, 0));
+        }
+
+        SendEvent(Character.Order.FinishAction);
+    }
+}
